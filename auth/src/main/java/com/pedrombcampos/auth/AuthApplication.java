12add62f5d1ee1914e82556848840c1d9afeee86
Pedro Campos <pedro.campos@startaideia.com.br>
2020-12-07 15:33:05 -0300

    Spring Security - Microservico Autenticacao

--- auth/src/main/java/com/pedrombcampos/auth/AuthApplication.java
+++ auth/src/main/java/com/pedrombcampos/auth/AuthApplication.java
@@ -1,13 +1,59 @@
 package com.pedrombcampos.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.pedrombcampos.auth.entity.Permission;
+import com.pedrombcampos.auth.entity.User;
+import com.pedrombcampos.auth.repository.PermissionRepository;
+import com.pedrombcampos.auth.repository.UserRepository;
 
 @SpringBootApplication
 public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(UserRepository userRepository, PermissionRepository permissionRepository,
+			BCryptPasswordEncoder passwordEncoder) {
+		return args -> {
+			initUsers(userRepository, permissionRepository, passwordEncoder);
+		};
+	}
+	
+	private void initUsers(UserRepository userRepository, PermissionRepository permissionRepository,
+			BCryptPasswordEncoder passwordEncoder) {
+		
+		Permission permission = null;
+		Permission findPermission = permissionRepository.findByDescription("Admin");
+		if (findPermission == null) {
+			permission = new Permission();
+			permission.setDescription("Admin");
+			permission = permissionRepository.save(permission);
+		} else {
+			permission = findPermission;
+		}
+		
+		User admin = new User();
+		admin.setUserName("pedrombcampos");
+		admin.setAccountNonExpired(true);
+		admin.setAccountNonLocked(true);
+		admin.setCredentialsNonExpired(true);
+		admin.setEnabled(true);
+		admin.setPassword(passwordEncoder.encode("123456"));
+		admin.setPermissions(Arrays.asList(permission));
+		
+		User find = userRepository.findByUserName("pedrombcampos");
+		if (find == null) {
+			userRepository.save(admin);
+		}
 	}
 
 }
